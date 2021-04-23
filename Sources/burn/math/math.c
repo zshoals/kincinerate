0395e33bf35af453d9aab0ba3df4404bbc09b70c
z <zshoals@gmail.com>
2021-04-23 16:26:37 -0500

    copy paste kinc math into burn math

--- Sources/burn/math/math.c
+++ Sources/burn/math/math.c
@@ -1,2 +1,92 @@
 #include <kinc/pch.h>
+#include <kinc/math/core.h>
 
+#include "math.h"
+
+float burn_math_tan(float x) {
+	return kinc_tanf(x);
+}
+
+float burn_math_cot(float x) {
+	return kinc_cot(x);
+}
+
+float burn_math_sin(float value) {
+	return kinc_sin(value);
+};
+
+float burn_math_cos(float value) {
+	return kinc_cos(value);
+};
+
+float burn_math_round(float value) {
+	return kinc_round(value);
+};
+
+float burn_math_ceil(float value) {
+	return kinc_ceil(value);
+};
+
+float burn_math_pow(float value, float exponent) {
+	return kinc_pow(value, exponent);
+};
+
+float burn_math_max_float(void) {
+	return kinc_max_float();
+};
+
+float burn_math_sqrt(float value) {
+	return kinc_sqrt(value);
+};
+
+float burn_math_abs(float value) {
+	return kinc_abs(value);
+};
+
+float burn_math_asin(float value) {
+	return kinc_asin(value);
+};
+
+float burn_math_acos(float value) {
+	return kinc_acos(value);
+};
+
+float burn_math_atan(float value) {
+	return kinc_atan(value);
+};
+
+float burn_math_atan2(float y, float x) {
+	return kinc_atan2(y, x);
+};
+
+float burn_math_floor(float value) {
+	return kinc_floor(value);
+};
+
+float burn_math_mod(float numer, float denom) {
+	return kinc_mod(numer, denom);
+};
+
+float burn_math_exp(float exponent) {
+	return kinc_exp(exponent);
+};
+
+float burn_math_min(float a, float b) {
+	return kinc_min(a, b);
+};
+
+float burn_math_max(float a, float b) {
+	return kinc_max(a, b);
+};
+
+int burn_math_mini(int a, int b) {
+	return kinc_mini(a, b);
+};
+
+int burn_math_maxi(int a, int b) {
+	return kinc_maxi(a, b);
+};
+
+float burn_math_clamp(float value, float minValue, float maxValue) {
+	return kinc_clamp(value, minValue, maxValue);
+};
--- Sources/burn/math/math.h
+++ Sources/burn/math/math.h
@@ -1,3 +1,30 @@
 #pragma once
 
 #include <kinc/pch.h>
+#include <kinc/math/core.h>
+
+#define KINC_PI 3.141592654
+#define KINC_TAU 6.283185307
+
+float burn_math_sin(float value);
+float burn_math_cos(float value);
+float burn_math_tan(float x);
+float burn_math_cot(float x);
+float burn_math_round(float value);
+float burn_math_ceil(float value);
+float burn_math_pow(float value, float exponent);
+float burn_math_max_float(void);
+float burn_math_sqrt(float value);
+float burn_math_abs(float value);
+float burn_math_asin(float value);
+float burn_math_acos(float value);
+float burn_math_atan(float value);
+float burn_math_atan2(float y, float x);
+float burn_math_floor(float value);
+float burn_math_mod(float numer, float denom);
+float burn_math_exp(float exponent);
+float burn_math_min(float a, float b);
+float burn_math_max(float a, float b);
+int burn_math_mini(int a, int b);
+int burn_math_maxi(int a, int b);
+float burn_math_clamp(float value, float minValue, float maxValue);
