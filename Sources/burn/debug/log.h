8820971c866c3792d330279770962798ef64a2fc
z <zshoals@gmail.com>
2021-04-24 12:35:21 -0500

    pretty up log formatting

--- Sources/burn/debug/log.h
+++ Sources/burn/debug/log.h
@@ -3,9 +3,9 @@
 //This is probably a bad sign
 //Strings already suck in C :)))))))
 #ifndef BURN_LOGGING_DISABLED
-	#define burn_log_info(format, ...) burn_internal_log_info("[INFO] " format, __VA_ARGS__)
-	#define burn_log_warn(format, ...) burn_internal_log_warn("[WARNING] " format, __VA_ARGS__)
-	#define burn_log_error(format, ...) burn_internal_log_error("[ERROR] " format, __VA_ARGS__)
+	#define burn_log_info(format, ...) burn_internal_log_info("[BURN--INFO]::" format, __VA_ARGS__)
+	#define burn_log_warn(format, ...) burn_internal_log_warn("[BURN--WARNING]::" format, __VA_ARGS__)
+	#define burn_log_error(format, ...) burn_internal_log_error("[BURN--ERROR]::" format, __VA_ARGS__)
 #else
 	#define burn_log_info(format, ...)
 	#define burn_log_warn(format, ...)
