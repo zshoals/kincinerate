21802d19f4ad5d0fd13d9f88abd19408ba727afb
z <zshoals@gmail.com>
2021-04-25 18:07:05 -0500

    framebuffer and window option init complete

--- Sources/burn/engine.c
+++ Sources/burn/engine.c
@@ -27,7 +27,7 @@ static void burn_private_engine_gameloop(void) {
 		burn_internal_keys_time_update(burn_time_dt_adjusted());
 	}
 	engine_state.update_callback(engine_state.logic_fixed_update_rate);
-	engine_state.render_callback(0.);
+	engine_state.render_callback(0.0);
 	//burn_private_engine_trigger_render
 	
 	//Interspersed with other random engine stuff?
@@ -47,10 +47,48 @@ static void burn_private_engine_initialize_background_callbacks(void) {
 	kinc_set_background_callback(&burn_internal_keys_set_all_up); //allow other people to hook into this?
 }
 
+static kinc_window_options_t burn_private_engine_construct_window_options(burn_engine_window_options_t *options) {
+	kinc_window_options_t wo;
+	wo.title = options->title;
+	wo.x = options->position_x;
+	wo.y = options->position_y;
+	wo.width = options->width;
+	wo.height = options->height;
+	wo.mode = options->mode;
+
+	int resizable = (options->resizable) ? KINC_WINDOW_FEATURE_RESIZEABLE : 0;
+	int minimizable = (options->minimizable) ? KINC_WINDOW_FEATURE_MINIMIZABLE : 0;
+	int maximizable = (options->maximizable) ? KINC_WINDOW_FEATURE_MAXIMIZABLE : 0;
+	int borderless = (options->borderless_window) ? KINC_WINDOW_FEATURE_BORDERLESS : 0;
+	int on_top = (options->always_on_top) ? KINC_WINDOW_FEATURE_ON_TOP : 0;
+	
+	int features = resizable | minimizable | maximizable | borderless | on_top;
+
+	wo.window_features = features;
+
+	//The following are not relevant to Kincinerate at this time. Leave at defaults.
+	wo.display_index = 0;
+	wo.visible = true;
+
+	return wo;
+}
+
+static kinc_framebuffer_options_t burn_private_engine_construct_framebuffer_options(burn_engine_window_options_t *options) {
+	kinc_framebuffer_options_t fbo;
+	//We don't care about a lot of the framebuffer settings atm.
+	kinc_internal_init_framebuffer_options(&fbo);
+
+	fbo.vertical_sync = options->vertical_synced;
+
+	return fbo;
+}
+
 void burn_engine_ignition(burn_engine_window_options_t *window_options, burn_engine_startup_options_t *startup_options) {
 	burn_log_info("Kincinerate initializing...");
 
-	kinc_init("Yes", 1024, 768, NULL, NULL);
+	kinc_window_options_t wo = burn_private_engine_construct_window_options(window_options);
+	kinc_framebuffer_options_t fbo = burn_private_engine_construct_framebuffer_options(window_options);
+	kinc_init(wo.title, wo.width, wo.height, &wo, &fbo);
 
 	{
 		memcpy(&window_state, window_options, sizeof(*window_options));
