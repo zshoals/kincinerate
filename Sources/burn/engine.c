608035d13df11c421dc870c7117b141ba8b78857
z <zshoals@gmail.com>
2021-04-25 18:08:36 -0500

    try and make this awfulness look ok

--- Sources/burn/engine.c
+++ Sources/burn/engine.c
@@ -103,32 +103,34 @@ void burn_engine_ignition(burn_engine_window_options_t *window_options, burn_eng
 };
 
 void burn_engine_window_options_init(
-burn_engine_window_options_t *options, 
-const char *title, 
-int position_x, int position_y, int width, int height,
-burn_window_mode mode, bool borderless_window, bool vertical_sync,
-bool resizable, bool minimizable, bool maximizable, bool always_on_top) {
-	options->title = title;
-	options->position_x = position_x;
-	options->position_y = position_y;
-	options->width = width;
-	options->height = height;
-	options->mode = mode;
-	options->borderless_window = borderless_window;
-	options->vertical_synced = vertical_sync;
-	options->resizable = resizable;
-	options->minimizable = minimizable;
-	options->maximizable = maximizable;
-	options->always_on_top = always_on_top;
+	burn_engine_window_options_t *options, 
+	const char *title, 
+	int position_x, int position_y, int width, int height,
+	burn_window_mode mode, bool borderless_window, bool vertical_sync,
+	bool resizable, bool minimizable, bool maximizable, bool always_on_top) {
+		
+		options->title = title;
+		options->position_x = position_x;
+		options->position_y = position_y;
+		options->width = width;
+		options->height = height;
+		options->mode = mode;
+		options->borderless_window = borderless_window;
+		options->vertical_synced = vertical_sync;
+		options->resizable = resizable;
+		options->minimizable = minimizable;
+		options->maximizable = maximizable;
+		options->always_on_top = always_on_top;
 };
 
 void burn_engine_startup_options_init(
-burn_engine_startup_options_t *options,
-double logic_fixed_update_rate,
-void (*update_callback)(double delta_time),
-void (*render_callback)(double extrapolation_alpha)) {
-	options->logic_fixed_update_rate = logic_fixed_update_rate;
-	options->update_callback = update_callback;
-	options->render_callback = render_callback;
+	burn_engine_startup_options_t *options,
+	double logic_fixed_update_rate,
+	void (*update_callback)(double delta_time),
+	void (*render_callback)(double extrapolation_alpha)) {
+
+		options->logic_fixed_update_rate = logic_fixed_update_rate;
+		options->update_callback = update_callback;
+		options->render_callback = render_callback;
 }
 
