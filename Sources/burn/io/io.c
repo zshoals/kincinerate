10f520d1decb19e96b64e3e531267d316b930107
z <zshoals@gmail.com>
2021-04-27 09:45:13 -0500

    update assert text more

--- Sources/burn/io/io.c
+++ Sources/burn/io/io.c
@@ -10,7 +10,7 @@ size_t burn_io_simple_asset_read(const char *filename, void *scratch_memory) {
 	kinc_file_reader_t reader;
 	bool success = kinc_file_reader_open(&reader, filename, KINC_FILE_TYPE_ASSET);
 	burn_log_warn("Loading asset: %s", filename);
-	assert(success && "Couldn't open the named asset. Did you provide the correct filename? Is the shader in the correct directory?");
+	assert(success && "Couldn't open the named asset. Did you provide the correct filename? Is the asset in the correct directory?");
 
 	size_t size = kinc_file_reader_size(&reader);
 	kinc_file_reader_read(&reader, scratch_memory, size);
