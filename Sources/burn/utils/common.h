21a80fad0c62ea183b80544edc8bffe9b3ed43ab
z <zshoals@gmail.com>
2021-04-27 00:00:58 -0500

    more of this awful macro

--- Sources/burn/utils/common.h
+++ Sources/burn/utils/common.h
@@ -10,11 +10,11 @@
 
 	Jesus christ this is so awful :D
 */
-#define BURN_SEARCH_HANDLE_IN_STRUCT_ARRAY(index_out, const_handle, scan_up_to, container, size) \
-	for (int i = 0; i < (size) && i < (scan_up_to); ++i) {										\
-		if (strcmp((const_handle), (container[i].handle)) == 0) {								\
-			(index) = i;																			\
-		}																						\
-	}																							\
-																								\
-	index = -1																					
+#define BURN_SEARCH_HANDLE_IN_STRUCT_ARRAY(index_out, const_handle, scan_up_to, container, size) 	\
+	index_out = -1;																					\
+																									\
+	for (int i = 0; i < (size) && i < (scan_up_to); ++i) {											\
+		if (strcmp((const_handle), (container[i].handle)) == 0) {									\
+			(index_out) = i;																		\
+		}																							\
+	}	
