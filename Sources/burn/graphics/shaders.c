27b2e50d2548f666af384dac4d2d965fb8c2e5e2
z <zshoals@gmail.com>
2021-04-28 13:47:10 -0500

    pipeline creation should work properly now

--- Sources/burn/graphics/shaders.c
+++ Sources/burn/graphics/shaders.c
@@ -43,6 +43,44 @@ void burn_shaders_load_and_store_frag_shader(burn_shaders_storage_t *storage, co
 	assert((storage->frag_active_slots < MAX_SHADERS) && "Too many fragment shaders added, raise the max or add less");
 };
 
+burn_shaders_pipeline_t *burn_shaders_pipeline_create(burn_shaders_storage_t *storage, const char *handle) {
+	burn_shaders_pipeline_t *location = &(storage->pipelines[storage->pipelines_active_slots]);
+	kinc_g4_pipeline_init(&location->pipeline);
+	storage->pipelines_active_slots++;
+	strcpy(location->handle, handle);
+
+	return location;
+};
+
+void burn_shaders_pipeline_set_depth_configuration(burn_shaders_pipeline_t *pipeline, bool depth_write_enabled, kinc_g4_compare_mode_t depth_mode) {
+	pipeline->pipeline.depth_write = depth_write_enabled;
+	pipeline->pipeline.depth_mode = depth_mode;
+};
+
+//!TODO: bad interface? just look up from storage? Eh :/?
+void burn_shaders_pipeline_set_vertex_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_vert_t *shader) {
+	pipeline->pipeline.vertex_shader = shader;
+};
+
+void burn_shaders_pipeline_set_fragment_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_frag_t *shader) {
+	pipeline->pipeline.vertex_shader = shader;
+};
+
+void burn_shaders_pipeline_set_blend_mode(burn_shaders_pipeline_t *pipeline, kinc_g4_blending_operation_t source, kinc_g4_blending_operation_t destination) {
+	pipeline->pipeline.blend_source = source;
+	pipeline->pipeline.blend_destination = destination;
+};
+
+void burn_shaders_pipeline_set_alpha_blend_mode(burn_shaders_pipeline_t *pipeline, kinc_g4_blending_operation_t source, kinc_g4_blending_operation_t destination) {
+	pipeline->pipeline.alpha_blend_source = source;
+	pipeline->pipeline.alpha_blend_destination = destination;
+};
+
+void burn_shaders_pipeline_compile(burn_shaders_storage_t *storage, burn_shaders_pipeline_t *pipeline) {
+
+	kinc_g4_pipeline_compile(&pipeline->pipeline);
+};
+
 burn_shaders_vert_t *burn_shaders_find_vert_shader(burn_shaders_storage_t *storage, const char *handle) {
 	int index = burn_search_handle_in_struct_array_burn_shaders_vert_t(storage->vertex_shaders, handle, storage->vert_active_slots, MAX_SHADERS);
 
--- Sources/burn/graphics/shaders.h
+++ Sources/burn/graphics/shaders.h
@@ -29,7 +29,7 @@ typedef struct burn_shaders_pipeline {
 	char handle[MAX_HANDLE_SIZE];
 	//burn_shaders_constant_location_t constant_locations[16];
 	//burn_shaders_texture_unit_t texture_unit[8];
-	kinc_g4_pipeline_t *pipeline;
+	kinc_g4_pipeline_t pipeline;
 } burn_shaders_pipeline_t;
 
 typedef struct burn_shaders_storage {
@@ -50,21 +50,21 @@ void burn_shaders_init(burn_shaders_storage_t *storage, size_t temp_memory_size)
 	Note that this cannot be undone.
 */
 void burn_shaders_free_temp_storage(burn_shaders_storage_t *storage);
-//Bool for returning if the file failed to be loaded, in which case the process is aborted.
+
 void burn_shaders_load_and_store_vert_shader(const char *filename, const char *new_handle);
 void burn_shaders_load_and_store_frag_shader(const char *filename, const char *new_handle);
-burn_shaders_vert_t *burn_shaders_find_vert_shader(const char *handle);
-burn_shaders_frag_t *burn_shaders_find_frag_shader(const char *handle);
 
 //!TODO: Unfinished pipeline stuff
-//!TODO: Set those active slots to 0
-void burn_shaders_pipeline_configuration_init(burn_shaders_pipeline_t *pipeline);
+burn_shaders_pipeline_t *burn_shaders_pipeline_create(burn_shaders_storage_t *storage, const char *handle);
 void burn_shaders_pipeline_set_depth_configuration(burn_shaders_pipeline_t *pipeline, bool depth_write_enabled, kinc_g4_compare_mode_t depth_mode);
-void burn_shaders_pipeline_set_vertex_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_vert_t shader);
-void burn_shaders_pipeline_set_fragment_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_frag_t shader);
+void burn_shaders_pipeline_set_vertex_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_vert_t *shader);
+void burn_shaders_pipeline_set_fragment_shader(burn_shaders_pipeline_t *pipeline, burn_shaders_frag_t *shader);
 void burn_shaders_pipeline_set_blend_mode(burn_shaders_pipeline_t *pipeline, kinc_g4_blending_operation_t source, kinc_g4_blending_operation_t destination);
 void burn_shaders_pipeline_set_alpha_blend_mode(burn_shaders_pipeline_t *pipeline, kinc_g4_blending_operation_t source, kinc_g4_blending_operation_t destination);
-void burn_shaders_pipeline_compile_and_store(burn_shaders_pipeline_t *pipeline, const char *new_handle);
+void burn_shaders_pipeline_compile_and_store(burn_shaders_pipeline_t *pipeline);
+
+burn_shaders_vert_t *burn_shaders_find_vert_shader(const char *handle);
+burn_shaders_frag_t *burn_shaders_find_frag_shader(const char *handle);
 burn_shaders_pipeline_t *burn_shaders_find_pipeline(const char *handle);
 //We need an abstraction for the creation of pipelines
 //This is all wrong. As a user, we shouldn't even be taking shaders OUT of this...all work should be done in here!
